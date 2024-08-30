58bc07ae7ca007be8de1cba4e69da67548a25193
Jaspreet <jaspreetkn92@gmail.com>
2024-08-30 08:11:07 +0100

    added the nav links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,23 @@
+import { NAV_LINKS } from '@/constants';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between">
+    <nav className="flex justify-between p-8">
       <Image src="/hilink-logo.svg" alt="travel camp" width={70} height={70} />
+
+      <div className="hidden lg:flex gap-12">
+        {NAV_LINKS.map((link) => (
+          <Link
+            key={link.key}
+            href={link.href}
+            className="text-gray-500 hover:font-bold transition-all"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </div>
 
       <Image
         src="/menu.svg"
