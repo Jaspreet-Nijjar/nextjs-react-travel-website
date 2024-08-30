ce97e4247456b8e224da3e906f5d466002ab65b4
Jaspreet <jaspreetkn92@gmail.com>
2024-08-30 08:02:47 +0100

    added the logo and menu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,12 +2,15 @@ import Image from 'next/image';
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="flex justify-between">
+      <Image src="/hilink-logo.svg" alt="travel camp" width={70} height={70} />
+
       <Image
-        src="/hilink-logo.svg"
-        alt="travel camp"
-        width={100}
-        height={100}
+        src="/menu.svg"
+        alt="hamburger menu"
+        width={30}
+        height={30}
+        className="max-lg:block hidden cursor-pointer"
       />
     </nav>
   );
