138410e3d8f81cc08ebba9da485f751ea2ef478d
Jaspreet <jaspreetkn92@gmail.com>
2024-09-01 11:07:29 +0100

    added responsive styling

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,10 @@
-import { FOOTER_CONTACT_INFO, FOOTER_LINKS } from '@/constants';
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '@/constants';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="p-6">
+    <footer className="flex flex-col md:flex-row md:flex-wrap md:justify-around md:gap-12 p-6">
       <Link href="/">
         <Image
           src="hilink-logo.svg"
@@ -15,37 +15,43 @@ const Footer = () => {
         />
       </Link>
 
-      <div className="flex flex-wrap">
-        <div className="grid grid-cols-2">
-          {FOOTER_LINKS.map((columns) => (
-            <FooterColumn title={columns.title}>
-              <ul className="flex flex-col gap-2">
-                {columns.links.map((link) => (
-                  <Link
-                    key={link}
-                    href={link}
-                    className="text-gray-500 text-sm"
-                  >
-                    {link}
-                  </Link>
-                ))}
-              </ul>
-            </FooterColumn>
-          ))}
-        </div>
-
-        <div className="mt-12">
-          <FooterColumn title={FOOTER_CONTACT_INFO.title}>
-            <ul>
-              {FOOTER_CONTACT_INFO.links.map((contact) => (
-                <p className="mb-2">
-                  {contact.label}:{' '}
-                  <span className="font-bold">{contact.value}</span>
-                </p>
+      <div className="flex flex-col md:flex-row md:gap-14">
+        {FOOTER_LINKS.map((columns) => (
+          <FooterColumn title={columns.title}>
+            <ul className="flex flex-col gap-2 mb-4">
+              {columns.links.map((link) => (
+                <Link key={link} href={link} className="text-gray-500 text-sm">
+                  {link}
+                </Link>
               ))}
             </ul>
           </FooterColumn>
-        </div>
+        ))}
+      </div>
+
+      <div>
+        <FooterColumn title={FOOTER_CONTACT_INFO.title}>
+          <ul>
+            {FOOTER_CONTACT_INFO.links.map((contact) => (
+              <p className="mb-2">
+                {contact.label}:{' '}
+                <span className="font-bold">{contact.value}</span>
+              </p>
+            ))}
+          </ul>
+        </FooterColumn>
+      </div>
+
+      <div>
+        <FooterColumn title={SOCIALS.title}>
+          <ul className="flex gap-2">
+            {SOCIALS.links.map((link) => (
+              <Link href="/">
+                <Image src={link} alt={link} width={24} height={24} />
+              </Link>
+            ))}
+          </ul>
+        </FooterColumn>
       </div>
     </footer>
   );
