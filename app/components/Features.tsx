256c1ebdaeaa216eb3dcbe1c51a4f38ba06b8f0c
Jaspreet <jaspreetkn92@gmail.com>
2024-09-07 19:42:55 +0100

    adding responsive styling to the content

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -3,11 +3,11 @@ import Image from 'next/image';
 
 const Features = () => {
   return (
-    <section className="flex flex-col p-6 bg-[url('/feature-bg.png')] bg-no-repeat bg-center">
+    <section className="grid grid-cols-1 bg-[url('/feature-bg.png')] bg-no-repeat bg-center">
       <Image src="/camp.svg" alt="camp" width={50} height={100} />
       <h1 className="text-4xl font-bold">Our Features</h1>
 
-      <ul>
+      <ul className="grid grid-cols-1 md:grid-cols-2 w-[50%] gap-6">
         {FEATURES.map((feature) => (
           <div className="mt-8 mb-6">
             <Image
@@ -20,7 +20,9 @@ const Features = () => {
             <h3 className="text-xl font-bold mb-2 capitalize">
               {feature.title}
             </h3>
-            <p className="text-gray-500 bg-white">{feature.description}</p>
+            <p className="text-gray-500 bg-white md:max-w-sm">
+              {feature.description}
+            </p>
           </div>
         ))}
       </ul>
