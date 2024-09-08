5253e721d26d8ec30bc3922e4d5afe89fce2b105
Jaspreet <jaspreetkn92@gmail.com>
2024-09-08 20:25:40 +0100

    added background image

--- app/components/Camp.tsx
+++ app/components/Camp.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 const Camp = () => {
   return (
-    <section>
-      <div>
+    <section className="bg-[url('/img-1.png')] bg-no-repeat bg-top-right py-32 text-white">
+      <div className="flex gap-6 px-6">
         <Image
           src="folded-map.svg"
           alt="map"
@@ -11,6 +11,11 @@ const Camp = () => {
           height={100}
           className="bg-green-500 rounded-full p-2"
         />
+
+        <div>
+          <h3 className="font-bold">Putuk Truno Camp</h3>
+          <p className="text-sm">Prigen, Pasuruan</p>
+        </div>
       </div>
     </section>
   );
