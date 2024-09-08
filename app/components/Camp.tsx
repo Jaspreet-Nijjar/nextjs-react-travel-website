81605da82c22bc097b4f6d92a9626f9c7916e07e
Jaspreet <jaspreetkn92@gmail.com>
2024-09-08 20:19:05 +0100

    added image

--- app/components/Camp.tsx
+++ app/components/Camp.tsx
@@ -1,5 +1,19 @@
+import Image from 'next/image';
+
 const Camp = () => {
-  return <div>Camp</div>;
+  return (
+    <section>
+      <div>
+        <Image
+          src="folded-map.svg"
+          alt="map"
+          width={50}
+          height={100}
+          className="bg-green-500 rounded-full p-2"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Camp;
