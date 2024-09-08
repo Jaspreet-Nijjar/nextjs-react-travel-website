56cca3674a6a6b34ca7246b10df8435a02b5bc6f
Jaspreet <jaspreetkn92@gmail.com>
2024-09-08 20:36:26 +0100

    designed the list of people to overlap

--- app/components/Camp.tsx
+++ app/components/Camp.tsx
@@ -1,11 +1,14 @@
+'use client';
+
+import { PEOPLE_URL } from '@/constants';
 import Image from 'next/image';
 
 const Camp = () => {
   return (
     <section className="bg-[url('/img-1.png')] bg-no-repeat bg-top-right py-32 text-white">
-      <div className="flex gap-6 px-6">
+      <div className="flex gap-6">
         <Image
-          src="folded-map.svg"
+          src="/folded-map.svg"
           alt="map"
           width={50}
           height={100}
@@ -16,6 +19,19 @@ const Camp = () => {
           <h3 className="font-bold">Putuk Truno Camp</h3>
           <p className="text-sm">Prigen, Pasuruan</p>
         </div>
+      </div>
+
+      <div className="flex items-center gap-4 px-8">
+        {PEOPLE_URL.map((person) => (
+          <Image
+            src={person}
+            alt={person}
+            width={52}
+            height={52}
+            className="ml-[-30px]"
+          />
+        ))}
+        <span className="font-bold text-white">50+ Joined</span>
       </div>
     </section>
   );
