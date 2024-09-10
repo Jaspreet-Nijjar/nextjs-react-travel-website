b5f8fd14827d2360e7f36dfafce68709eef7672c
Jaspreet <jaspreetkn92@gmail.com>
2024-09-10 08:42:47 +0100

    added context box

--- app/components/Camp.tsx
+++ app/components/Camp.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 const Camp = () => {
   return (
-    <section className="bg-[url('/img-1.png')] bg-no-repeat bg-top-right py-6 px-8 text-white h-[340px]">
+    <section className="bg-[url('/img-1.png')] bg-no-repeat bg-top-right py-6 px-8 text-white h-[340px] mb-52">
       <div className="flex flex-col justify-between h-full">
         <div className="flex gap-6">
           <Image
@@ -34,6 +34,17 @@ const Camp = () => {
           ))}
           <span className="font-bold text-white">50+ Joined</span>
         </div>
+      </div>
+
+      <div className="p-6 bg-green-700 mt-12 mb-12 rounded-lg">
+        <h3 className="text-[24px]">
+          <strong>Feeling Lost</strong> And Not Knowing The Way?
+        </h3>
+        <p className="text-sm mt-6">
+          Starting from the anxiety of the climbers when visiting a new climbing
+          location, the possibility of getting lost is very large. That's why we
+          are here for those of you who want to start an adventure
+        </p>
       </div>
     </section>
   );
